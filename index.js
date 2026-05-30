09aa05a79d32181b5c9ec986d434d4495de05e73
Jaideep Vantipalli <jaideepvantipalli@gmail.com>
2026-05-30 21:07:47 +0530

    Event listerners added ,css added

--- index.js
+++ index.js
@@ -29,7 +29,23 @@ function addtask(){
 function renderTodoList(){
     let todolistHTML='';
 
-    for(let i=0;i<todoList.length;i++){
+    todoList.forEach((task,index)=>{
+        const {name,duedate}=task;
+        const html=`
+        <div>${name}</div>
+        <div>${duedate}</div>
+        <button class="delete-button js-delete-todo-button">Delete</button>
+        `;
+        todolistHTML+=html;
+    });
+    document.querySelector('.todo-list').innerHTML=todolistHTML;
+    document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton,index)=>{
+        deleteButton.addEventListener('click',()=>{
+            todoList.splice(index,1);
+            renderTodoList();
+        });
+    });
+    /*for(let i=0;i<todoList.length;i++){
         const task=todoList[i];
         //const name=task.name;
         //const duedate=task.duedate;
@@ -44,5 +60,9 @@ function renderTodoList(){
         `;
         todolistHTML+=html;
     }
-    document.querySelector('.todo-list').innerHTML=todolistHTML;
+    document.querySelector('.todo-list').innerHTML=todolistHTML;*/
 }
+
+document.querySelector('.js-add-task').addEventListener('click',()=>{
+    addtask();
+});
